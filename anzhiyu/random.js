b62f5acff871b66329286c78910202ae25445b08
我先起床了 <doooio@outlook.com>
2023-05-20 17:18:17 +0800

    Site updated: 2023-05-20 17:18:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/20/hello-world/","2023/05/20/Git基本使用/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/20/Git基本使用/","2023/05/20/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
